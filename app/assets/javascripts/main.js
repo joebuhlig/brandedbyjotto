a7b843b42bd88559d97b8d040b781b5e88267649
Joe Buhlig <joe@joebuhlig.com>
2015-11-18 13:55:04 -0600

    Cleaned up spacing of navbar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,12 +9,14 @@ $(document).on('page:change', function(event) {
 	        $(".navbar-nav > li > a").addClass("scrolled", 750);
 			$(".navbar .glyphicon").addClass("scrolled", 750);
 			$(".navbar-toggle").addClass("scrolled", 750);
+			$(".navbar-collapse").addClass("scrolled", 750);
 	    }
 	    else {
 	        $(".navbar").removeClass("scrolled", 750);
 	        $(".navbar-nav > li > a").removeClass("scrolled", 750);
 	        $(".navbar .glyphicon").removeClass("scrolled", 750);
 	        $(".navbar-toggle").removeClass("scrolled", 750);
+	        $(".navbar-collapse").removeClass("scrolled", 750);
 	    }
 	});
 });
