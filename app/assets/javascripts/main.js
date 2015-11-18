1493fef6732cc828c70a0f531bbc2e05f7e6acd4
Joe Buhlig <joe@joebuhlig.com>
2015-11-18 15:07:30 -0600

    Updated colors on navigation and added hero image

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,17 +6,21 @@ $(document).on('page:change', function(event) {
 	    var topOfWindow = $(window).scrollTop() + $('.navbar').height();
 	    if (topOfContent < topOfWindow) {
 	        $(".navbar").addClass("scrolled", 750);
-	        $(".navbar-nav > li > a").addClass("scrolled", 750);
+	        $(".nav-title").addClass("scrolled", 750);
 			$(".navbar .glyphicon").addClass("scrolled", 750);
 			$(".navbar-toggle").addClass("scrolled", 750);
 			$(".navbar-collapse").addClass("scrolled", 750);
+			$(".white-logo").addClass("scrolled", 750);
+			$(".color-logo").addClass("scrolled", 750);
 	    }
 	    else {
 	        $(".navbar").removeClass("scrolled", 750);
-	        $(".navbar-nav > li > a").removeClass("scrolled", 750);
+	        $(".nav-title").removeClass("scrolled", 750);
 	        $(".navbar .glyphicon").removeClass("scrolled", 750);
 	        $(".navbar-toggle").removeClass("scrolled", 750);
 	        $(".navbar-collapse").removeClass("scrolled", 750);
+			$(".white-logo").removeClass("scrolled", 750);
+			$(".color-logo").removeClass("scrolled", 750);
 	    }
 	});
 });
